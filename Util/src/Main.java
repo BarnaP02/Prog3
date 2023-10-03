82d9334ddf6ea14bdd5dd5ea2e3c531e80ee692f
unknown <176058@BME.HU>
2023-10-03 13:01:36 +0200

    kesz a 8asig

--- Util/src/Main.java
+++ Util/src/Main.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,6 +26,9 @@ public class Main {
             else if (cmd[0].equals("list")){ list(lista, cmd); }
             else if (cmd[0].equals("load")){ load(lista, cmd[1]); }
             else if (cmd[0].equals("save")){ save(lista, cmd[1]); }
+            else if (cmd[0].equals("search")){ search(lista, cmd); }
+            else if (cmd[0].equals("find")){ find(lista, cmd); }
+            else if (cmd[0].equals("delete")){ delete(lista, cmd[1]); }
         }
     }
     protected static void add(String[] cmd, ArrayList<Beer> lista){
@@ -101,6 +105,68 @@ public class Main {
                 e.printStackTrace();
                 // Handle other IOExceptions
             }
+        }
+    }
+    protected static void search(ArrayList<Beer> lista, String[] cmd){
+		if(cmd.length==2){
+		    for (Beer listaelem : lista){
+		    	if(listaelem.getName().equals(cmd[1]))
+		        System.out.println(listaelem.toString());
+		    }    		
+    	}
+		else{
+		    for (Beer listaelem : lista){
+			    if(cmd[1].equals("name")){
+			    	if(listaelem.getName().equals(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+			    if(cmd[1].equals("style")){
+			    	if(listaelem.getStyle().equals(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+			    if(cmd[1].equals("strength")){
+			    	if(listaelem.getAlkoholfok() == Double.parseDouble(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+		    }    		
+		}
+    }
+    protected static void find(ArrayList<Beer> lista, String[] cmd){
+		if(cmd.length==2){
+		    for (Beer listaelem : lista){
+		    	if(listaelem.getName().contains(cmd[1]))
+		        System.out.println(listaelem.toString());
+		    }    		
+    	}
+		else{
+		    for (Beer listaelem : lista){
+			    if(cmd[1].equals("name")){
+			    	if(listaelem.getName().contains(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+			    if(cmd[1].equals("style")){
+			    	if(listaelem.getStyle().contains(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+			    if(cmd[1].equals("strength")){
+			    	if(listaelem.getAlkoholfok() >= Double.parseDouble(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+			    if(cmd[1].equals("weaker")){
+			    	if(listaelem.getAlkoholfok() <= Double.parseDouble(cmd[2]))
+			        System.out.println(listaelem.toString());		    		
+		    	}
+		    }    		
+		}
+    }
+    protected static void delete(ArrayList<Beer> lista, String szemet){
+    	Iterator<Beer> it = lista.iterator();
+        while(it.hasNext()) {
+          Beer i = it.next();
+          if(i.getName().equals(szemet)) {
+            it.remove();
+            System.out.println("The item has been deleted.");
+          }
         }
     }
 }
